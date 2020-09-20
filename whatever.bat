c6e6a5e204b5fc6250b73b30bd5bc96610901dc5
otisgbangba <jotitolaiye@hotmail.co.uk>
2020-09-20 21:49:13 +0200

    Add files via upload

---
+++ whatever.bat
@@ -0,0 +1,19 @@
+@echo off
+title WARNING
+echo PROGRAM Has INSTALLED MALICIOUS SOFTWARE
+pause
+echo CONTACTING ANTIVIRUS SOFTWARE...
+pause
+echo DO YOU WISH TO DELETE VIRUS?...(y/n)
+pause
+echo ACCESS DENIED 
+pause >nul
+echo ACCESS DENIED 
+pause >nul
+echo ACCESS DENIED
+pause >nul
+echo RUNNING VIRUS...
+pause
+:i
+dir/a
+goto i
